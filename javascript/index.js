52d4a9e0127952a3d2996989b0bb6e480866c8f3
HyeokJoon Kong <orijoon98@gmail.com>
2022-02-09 16:12:13 +0900

    Feat: 랜덤 파라미터 생성 기능

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
 const http = require('./http');
+const creator = require('./random');
 
 http.post('web3_clientVersion', []);
---
+++ javascript/random.js
@@ -0,0 +1,31 @@
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const hexCharacters = 'abcdef0123456789';
+const tags = ['latest', 'earliest', 'pending'];
+
+exports.randomString = (size) => {
+  let result = '';
+  for (let i = 0; i < size; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+exports.randomHex = (size) => {
+  let result = '0x';
+  for (let i = 0; i < size; i++) {
+    result += hexCharacters.charAt(
+      Math.floor(Math.random() * hexCharacters.length)
+    );
+  }
+  return result;
+};
+
+exports.randomBoolean = () => {
+  if (Math.floor(Math.random() * 2)) return true;
+  return false;
+};
+
+exports.randomTag = () => {
+  return tags[Math.floor(Math.random() * tags.length)];
+};
