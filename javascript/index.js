731554594c0649e4ca98b179a1db52b50f7014be
HyeokJoon Kong <orijoon98@gmail.com>
2022-02-10 10:58:26 +0900

    Feat: object quantity|tag 파라미터 테스트 추가

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,67 @@
 const test = require('./test');
 
-test.dataQuantityTagRandom('eth_getBalance', 'hex', 20, 'tag', 1, 5);
-test.dataQuantityTag(
-  'eth_getBalance',
-  '0xabcf409adf429c4d925a0300ce9de34a870afff0',
-  'latest'
-);
+// test.dataQuantityTagRandom('eth_getBalance', 'hex', 20, 'tag', 1, 5);
+// test.dataQuantityTag(
+//   'eth_getBalance',
+//   '0xabcf409adf429c4d925a0300ce9de34a870afff0',
+//   'latest'
+// );
 
-test.dataQuantityTagRandom('eth_getTransactionCount', 'hex', 20, 'tag', 1, 5);
-test.dataQuantityTag(
-  'eth_getTransactionCount',
-  '0xabcf409adf429c4d925a0300ce9de34a870afff0',
-  'latest'
-);
+// test.dataQuantityTagRandom('eth_getTransactionCount', 'hex', 20, 'tag', 1, 5);
+// test.dataQuantityTag(
+//   'eth_getTransactionCount',
+//   '0xabcf409adf429c4d925a0300ce9de34a870afff0',
+//   'latest'
+// );
+
+// test.objectQuantityTagRandom(
+//   'eth_call',
+//   'hex',
+//   20,
+//   'hex',
+//   20,
+//   null,
+//   null,
+//   null,
+//   'hex',
+//   20,
+//   'tag',
+//   1,
+//   5
+// );
+// test.objectQuantityTag(
+//   'eth_call',
+//   null,
+//   '0xABCf409ADf429C4D925A0300cE9DE34A870aFfF0',
+//   null,
+//   null,
+//   null,
+//   null,
+//   'latest'
+// );
+
+// test.objectQuantityTagRandom(
+//   'eth_estimateGas',
+//   null,
+//   20,
+//   null,
+//   20,
+//   null,
+//   null,
+//   null,
+//   null,
+//   20,
+//   null,
+//   1,
+//   5
+// );
+// test.objectQuantityTag(
+//   'eth_estimateGas',
+//   null,
+//   null,
+//   null,
+//   null,
+//   null,
+//   null,
+//   null
+// );
--- javascript/test.js
+++ javascript/test.js
@@ -57,3 +57,136 @@ exports.dataQuantityTag = (
   params = [data, quantityTag];
   http.post(method, params);
 };
+
+// eth_call
+// eth_estimateGas
+// Random Test
+// 1. Object
+// 2. Quantity|Tag
+exports.objectQuantityTagRandom = (
+  method, // string
+  fromType, // hex, string, null
+  fromByte, // number
+  toType, // hex, string, null
+  toByte, // number,
+  maxGas, // number, null
+  maxGasPrice, // number, null
+  maxValue, // number, null
+  dataType, // hex, string, null
+  dataByte, // number
+  quantityTagType, // quantity, tag, null
+  maxQuantity, // number
+  testCount // number
+) => {
+  for (let i = 0; i < testCount; i++) {
+    let from, to, gas, gasPrice, value, data, quantityOrTag, params;
+    switch (fromType) {
+      case 'hex':
+        from = creator.randomHex(2 * fromByte);
+        break;
+      case 'string':
+        from = creator.randomString(fromByte);
+        break;
+      case null:
+        from = null;
+        break;
+      default:
+        log.error('파라미터 입력값을 확인해주세요.');
+    }
+    switch (toType) {
+      case 'hex':
+        to = creator.randomHex(2 * toByte);
+        break;
+      case 'string':
+        to = creator.randomString(toByte);
+        break;
+      case null:
+        to = null;
+        break;
+      default:
+        log.error('파라미터 입력값을 확인해주세요.');
+    }
+    if (maxGas == null) {
+      gas = null;
+    } else {
+      gas = creator.randomQuantity(maxGas);
+    }
+    if (maxGasPrice == null) {
+      gasPrice = null;
+    } else {
+      gasPrice = creator.randomQuantity(maxGasPrice);
+    }
+    if (maxValue == null) {
+      value = null;
+    } else {
+      value = creator.randomQuantity(maxValue);
+    }
+    switch (dataType) {
+      case 'hex':
+        data = creator.randomHex(2 * dataByte);
+        break;
+      case 'string':
+        data = creator.randomString(dataByte);
+        break;
+      case null:
+        data = null;
+        break;
+      default:
+        log.error('파라미터 입력값을 확인해주세요.');
+    }
+    switch (quantityTagType) {
+      case 'quantity':
+        quantityOrTag = creator.randomQuantity(maxQuantity);
+        break;
+      case 'tag':
+        quantityOrTag = creator.randomTag();
+        break;
+      case null:
+        quantityOrTag = null;
+        break;
+      default:
+        log.error('파라미터 입력값을 확인해주세요.');
+    }
+    params = [
+      {
+        from: from,
+        to: to,
+        gas: gas,
+        gasPrice: gasPrice,
+        value: value,
+        data: data,
+      },
+      quantityOrTag,
+    ];
+    http.post(method, params);
+  }
+};
+
+// eth_call
+// eth_estimateGas
+// Test
+// 1. Object
+// 2. Quantity|Tag
+exports.objectQuantityTag = (
+  method, // string
+  from,
+  to,
+  gas,
+  gasPrice,
+  value,
+  data,
+  quantityTag
+) => {
+  params = [
+    {
+      from: from,
+      to: to,
+      gas: gas,
+      gasPrice: gasPrice,
+      value: value,
+      data: data,
+    },
+    quantityTag,
+  ];
+  http.post(method, params);
+};
